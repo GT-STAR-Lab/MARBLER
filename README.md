49edb066a91503dfbb428fa4b2ecc261ca3f8e93
Meher Shashwat Nigam <mehershashwat@gmail.com>
2023-02-19 16:28:52 -0500

    Added README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Realistic Benchmarks for Collaborative Heterogeneous Multi-Robot Systems
+#### Team: Reza Torbati, Shubham Lohiya, Shivika Singh, Meher Nigam
+## Environment, agent setup
+### Robotarium 
+- Github - https://github.com/robotarium/robotarium_python_simulator
+- Youtube playlist- Networked Robotics - YouTube
+
--- envs/utilities.py
+++ envs/utilities.py
@@ -4,13 +4,12 @@ import random
 def is_close( agent_poses, agent_index, prey_loc, sensing_radius):
     agent_pose = agent_poses[:2, agent_index]
     prey_loc = prey_loc.reshape((1,2))[0]
-    # print(agent_pose, prey_loc, np.linalg.norm(agent_pose - prey_loc), sensing_radius)
     return np.linalg.norm(agent_pose - prey_loc) <= sensing_radius
 
 def get_random_vel():
     '''
         The first row is the linear velocity of each robot in meters/second (range +- .03-.2)
-        The second row is the angular velocity of each robot in radians/second (range +- .1-)
+        The second row is the angular velocity of each robot in radians/second
     '''
     linear_vel = random.uniform(0.05,0.2) 
     #* (1 if random.uniform(-1,1)>0 else -1)
