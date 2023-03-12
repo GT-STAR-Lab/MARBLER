0ce2cf85f6c9e704fb706539a39a7435997d88a7
Shivika Singh <shivikasingh275@gmail.com>
2023-03-12 14:02:14 -0400

    Adding multiagentenv.py file

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,9 +1,9 @@
 REGISTRY = {}
 
-from .basic_controller import BasicMAC
-from .non_shared_controller import NonSharedMAC
+# from .basic_controller import BasicMAC
+# from .non_shared_controller import NonSharedMAC
 from .maddpg_controller import MADDPGMAC
 
-REGISTRY["basic_mac"] = BasicMAC
-REGISTRY["non_shared_mac"] = NonSharedMAC
+# REGISTRY["basic_mac"] = BasicMAC
+# REGISTRY["non_shared_mac"] = NonSharedMAC
 REGISTRY["maddpg_mac"] = MADDPGMAC
