b56e969c7594940eb654751a1ab5a252346b647d
Meher Shashwat Nigam <mehershashwat@gmail.com>
2023-04-20 00:27:44 -0400

    Recatored code with better imports, clean main and argparse for scenario selection

--- multiagent/main.py
+++ multiagent/main.py
@@ -1,74 +1,21 @@
-import torch
-import json
-import importlib
-import numpy as np
 import yaml
-import os,sys
-# sys.path.insert(1, os.path.join(sys.path[0], '..'))
-module_dir = os.path.dirname(__file__)
-config_path = os.path.join('./multiagent/scenarios/pcp/config.yaml')
+import os
+from multiagent.robotarium_env.utilities import run_env, objectview
+import argparse
 
-class ObjectView(object):
-    def __init__(self, d):
-        self.__dict__ = d
 
-def load_model(args):
-    print(args.model_config_file)
-    model_config = os.path.join(module_dir, args.model_config_file)
-    print(model_config)
-    model_config = ObjectView(json.load(open(model_config)))
-    model_config.n_actions = args.n_actions
-    params = torch.load(os.path.join(module_dir, args.model_file), map_location=torch.device('cpu'))
-    input_dim = params[list(params.keys())[0]].shape[1]
+def main():
+    module_dir = os.path.dirname(__file__)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--scenario', type=str, default='pcp', help='scenario name')
+    args = parser.parse_args()
 
-    actor = importlib.import_module(f'multiagent.robotarium_env.{args.actor_file}')
-    actor = getattr(actor, args.actor_class)
-    
-    model = actor(input_dim, model_config)
-    model.load_state_dict(params)
+    config_path = os.path.join(module_dir, "scenarios", args.scenario, "config.yaml")
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+    config = objectview(config)
 
-    #print(model.eval())
-    return model, model_config
+    run_env(config, module_dir)
 
-with open(config_path, 'r') as f:
-    config = yaml.safe_load(f)
-
-args = ObjectView(config)
-model, model_config = load_model(args)
-
-env_module = importlib.import_module(f'multiagent.scenarios.pcp.{args.env_file}')
-env_class = getattr(env_module, args.env_class)
-env = env_class(args)
-
-obs = np.array(env.reset())
-n_agents = len(obs)
-hs = [np.zeros((model_config.hidden_dim, )) for i in range(n_agents)]
-
-totalReward = 0
-totalSteps = 0
-for i in range(args.episodes):
-    episodeReward = 0
-    episodeSteps = 0
-    for j in range(args.max_episode_steps):      
-        if model_config.obs_agent_id: #Appends the agent id if obs_agent_id is true. TODO: support obs_last_action too
-            obs = np.concatenate([obs,np.eye(n_agents)], axis=1)
-
-        #Gets the q values and then the action from the q values
-        q_values, hs = model(torch.Tensor(obs), torch.Tensor(hs))
-        actions = np.argmax(q_values.detach().numpy(), axis=1)
-
-        obs, reward, done, _ = env.step(actions)
-        episodeReward += reward[0]
-        if done[0]:
-            episodeSteps = j+1
-            break
-    if episodeSteps == 0:
-        episodeSteps = args.max_episode_steps
-    obs = np.array(env.reset())
-    print('Episode', i+1)
-    print('Episode reward:', episodeReward)
-    print('Episode steps:', episodeSteps)
-    totalReward += episodeReward
-    totalSteps += episodeSteps
-print('\nTotal Reward:',totalReward)
-print('Total Steps:', totalSteps)
+if __name__ == '__main__':
+    main()
